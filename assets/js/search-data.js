5ef12c3959ab24ed6eeee2a859a9cd270d675667
young-eun-nam <maradonam.ye@gmail.com>
2026-03-07 01:14:19 +0000

    Deploying to gh-pages from @ young-eun-nam/young-eun-nam.github.io@7faf491731b7bf8efe0c93deaa341d4fc3809016 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,21 +23,14 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "dropdown-bookshelf",
-              title: "bookshelf",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/books/";
-              },
-            },{id: "dropdown-blog",
-              title: "blog",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/blog/";
-              },
-            },{id: "post-a-post-with-plotly-js",
+        },{id: "nav-submenus",
+          title: "submenus",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/_pages/dropdown.html";
+          },
+        },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
         
