b090c1915ac55faebf4f55a1766a6f9baeead9a1
young-eun-nam <maradonam.ye@gmail.com>
2026-03-07 01:18:48 +0000

    Deploying to gh-pages from @ young-eun-nam/young-eun-nam.github.io@d7b549040cf9048483ab46acfb03549a55c734ba 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,13 +23,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "nav-submenus",
-          title: "submenus",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/_pages/dropdown.html";
-          },
         },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
