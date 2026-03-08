82fc3bf66a4dbe00f034c8436ee243d3277e9c05
young-eun-nam <maradonam.ye@gmail.com>
2026-03-08 07:36:12 +0000

    Deploying to gh-pages from @ young-eun-nam/young-eun-nam.github.io@2b82fed4208447fe172adeff619778fcc4bac88f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -440,21 +440,14 @@ ninja.data = [{
         title: 'CV',
         section: 'Socials',
         handler: () => {
-          window.open("/assets/pdf/example_pdf.pdf", "_blank");
+          window.open("/assets/pdf/Youngeun_Nam___CV.pdf", "_blank");
         },
       },{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
-        },
-      },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("mailto:%79%6F%75%6E%67%65%75%6E.%6E%61%6D@%6B%61%69%73%74.%61%63.%6B%72", "_blank");
         },
       },{
         id: 'social-rss',
@@ -468,14 +461,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://scholar.google.com/citations?user=Cz1zyRsAAAAJ", "_blank");
         },
       },{
       id: 'light-theme',
