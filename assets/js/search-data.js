0979a9c38f162c61335a1f85ffa105c0c987399a
young-eun-nam <maradonam.ye@gmail.com>
2026-03-11 14:22:26 +0000

    Deploying to gh-pages from @ young-eun-nam/young-eun-nam.github.io@14ec81d9a45d813035ef3e7ec1cde3b3a578069a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-teaching",
           title: "teaching",
-          description: "Course materials, schedules, and resources for classes taught.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/teaching/";
